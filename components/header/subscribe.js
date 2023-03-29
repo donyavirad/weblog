cb8cd6386759402b8eae0532b2be819af35fde87
hamed donyavi <85428934+donyavirad@users.noreply.github.com>
2023-03-29 22:00:34 +0330

    feat (subscribe): ✨ add subscribe component

---
+++ components/header/subscribe.js
@@ -0,0 +1,11 @@
+import Button from "@material-tailwind/react/components/Button";
+
+const Subscribe = () => {
+    return (
+        <div>
+            <Button className="bg-gradient-to-r from-green-brighter to-green-darker rounded shadow-none hover:shadow-none">Subscribe</Button>
+        </div>
+    );
+};
+
+export default Subscribe;
