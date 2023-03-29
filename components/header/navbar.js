12ee5b947a38e4aa8ae843885186dcff78685897
hamed donyavi <85428934+donyavirad@users.noreply.github.com>
2023-03-29 21:46:48 +0330

    feat (component/navbar): ✨ add navbar component

---
+++ components/header/navbar.js
@@ -0,0 +1,22 @@
+const Navbar = () => {
+    const menuItems = [
+        "Hotel Experiences",
+        "Best travel places",
+        "Cheap traveling ideas",
+        "Wildlife Safari",
+        "Best Street food",
+    ]
+    return (
+        <nav>
+            <ul className="flex space-x-4">
+                {menuItems.map((item,id) => (
+                    <li key={id} className="relative transition-all duration-300 font-medium text-gray-800 hover:text-green-darker hover:font-bold after:content-[''] after:absolute after:bottom-0 after:left-0 after:w-0 after:transition-all after:duration-300 after:h-0.5 after:bg-green-darker hover:after:w-1/2 ">
+                        {item}
+                    </li>
+                ))}
+            </ul>
+        </nav>
+    )
+  }
+  
+  export default Navbar
