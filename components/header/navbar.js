aae058663ef3739b404fcee73ed34883e344bc44
hamed donyavi <85428934+donyavirad@users.noreply.github.com>
2023-03-31 18:43:19 +0330

    style (navbar): :lipstick: add responsive style to navbar

--- components/header/navbar.js
+++ components/header/navbar.js
@@ -7,7 +7,7 @@ const Navbar = () => {
         "Best Street food",
     ]
     return (
-        <nav>
+        <nav className="hidden lg:block">
             <ul className="flex space-x-4">
                 {menuItems.map((item,id) => (
                     <li key={id} className="relative transition-all duration-300 font-Lato-Regular text-gray-800  cursor-pointer  hover:text-green-darker hover:font-Lato-Bold after:content-[''] after:absolute after:bottom-0 after:left-0 after:w-0 after:transition-all after:duration-300 after:h-0.5 after:bg-green-darker hover:after:w-1/2 ">
