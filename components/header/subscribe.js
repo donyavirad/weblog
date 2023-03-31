1dc16696d09b7f0253084bf5d834d65d1edb580b
hamed donyavi <85428934+donyavirad@users.noreply.github.com>
2023-03-31 19:11:24 +0330

    style (subscribe): :lipstick: change padding button

--- components/header/subscribe.js
+++ components/header/subscribe.js
@@ -3,7 +3,7 @@ import Button from "@material-tailwind/react/components/Button";
 const Subscribe = () => {
     return (
         <div>
-            <Button className="font-Montserrat-Medium bg-gradient-to-r from-green-brighter to-green-darker rounded shadow-none hover:shadow-none">Subscribe</Button>
+            <Button className="px-4 py-2 font-Montserrat-Medium bg-gradient-to-r from-green-brighter to-green-darker rounded shadow-none hover:shadow-none">Subscribe</Button>
         </div>
     );
 };
