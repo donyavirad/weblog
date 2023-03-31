08d44d5107c5538fc0807af90ce3d9d34b84deaa
hamed donyavi <85428934+donyavirad@users.noreply.github.com>
2023-03-31 17:49:46 +0330

    style (subscribe): :art: add font to subscribe component

--- components/header/subscribe.js
+++ components/header/subscribe.js
@@ -3,7 +3,7 @@ import Button from "@material-tailwind/react/components/Button";
 const Subscribe = () => {
     return (
         <div>
-            <Button className="bg-gradient-to-r from-green-brighter to-green-darker rounded shadow-none hover:shadow-none">Subscribe</Button>
+            <Button className="font-Montserrat-Medium bg-gradient-to-r from-green-brighter to-green-darker rounded shadow-none hover:shadow-none">Subscribe</Button>
         </div>
     );
 };
