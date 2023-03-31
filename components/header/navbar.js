29a6b5793618c534232ab4dd1ccbb316b73e36fd
hamed donyavi <85428934+donyavirad@users.noreply.github.com>
2023-03-31 17:44:36 +0330

    style (navbar): :art: add font lato in navbar component

--- components/header/navbar.js
+++ components/header/navbar.js
@@ -10,7 +10,7 @@ const Navbar = () => {
         <nav>
             <ul className="flex space-x-4">
                 {menuItems.map((item,id) => (
-                    <li key={id} className="relative transition-all duration-300 font-medium text-gray-800 hover:text-green-darker hover:font-bold after:content-[''] after:absolute after:bottom-0 after:left-0 after:w-0 after:transition-all after:duration-300 after:h-0.5 after:bg-green-darker hover:after:w-1/2 ">
+                    <li key={id} className="relative transition-all duration-300 font-Lato-Regular text-gray-800  cursor-pointer  hover:text-green-darker hover:font-Lato-Bold after:content-[''] after:absolute after:bottom-0 after:left-0 after:w-0 after:transition-all after:duration-300 after:h-0.5 after:bg-green-darker hover:after:w-1/2 ">
                         {item}
                     </li>
                 ))}
