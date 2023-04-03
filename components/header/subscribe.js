fde89d6dc60e61de4a88f94bbf28c13b222f576e
hamed donyavi <85428934+donyavirad@users.noreply.github.com>
2023-04-03 12:17:13 +0330

    refactor (subscribe): :zap: Change how subscribe button displays on mobile screen

--- components/header/subscribe.js
+++ components/header/subscribe.js
@@ -1,9 +1,17 @@
 import Button from "@material-tailwind/react/components/Button";
-
+import { useMediaQuery } from "../../hooks/useMediaQuery";
 const Subscribe = () => {
+    const mediaLg = useMediaQuery("lg");
+    if (mediaLg) {
+        return (
+            <div>
+                <Button className="px-4 py-2 font-Montserrat-Medium bg-gradient-to-r from-green-brighter to-green-darker rounded shadow-none hover:shadow-none">Subscribe</Button>
+            </div>
+        );
+    }
     return (
         <div>
-            <Button className="px-4 py-2 font-Montserrat-Medium bg-gradient-to-r from-green-brighter to-green-darker rounded shadow-none hover:shadow-none">Subscribe</Button>
+            <Button className="w-full py-2 font-Montserrat-Medium bg-gradient-to-r from-green-brighter to-green-darker rounded shadow-none hover:shadow-none">Subscribe</Button>
         </div>
     );
 };
