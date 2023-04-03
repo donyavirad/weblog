77b903da62ff37e88c0dc65661720de971a8aa21
hamed donyavi <85428934+donyavirad@users.noreply.github.com>
2023-04-03 13:26:12 +0330

    refactor (header,hamberger): :zap: add hamberger, Logo and Notification to header component and Menu burger works

--- components/header/hamberger.js
+++ components/header/hamberger.js
@@ -1,20 +1,20 @@
 import { IconButton } from "@material-tailwind/react";
 import { useMediaQuery } from "../../hooks/useMediaQuery";
 
-const Hamberger = ({ openNav }) => {
+const Hamberger = ({ openNav, setOpenNav }) => {
     const mediaLg = useMediaQuery("lg");
 
     if (!mediaLg)
         return (
             <div>
                 {openNav ? (
-                    <IconButton variant="text" ripple={false} className="hover:bg-transparent focus:bg-transparent active:bg-transparent">
+                    <IconButton onClick={() => setOpenNav(false)} variant="text" ripple={false} className="hover:bg-transparent focus:bg-transparent active:bg-transparent">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" className="h-6 w-6" viewBox="0 0 24 24" stroke="#000" strokeWidth={2}>
                             <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
                         </svg>
                     </IconButton>
                 ) : (
-                    <IconButton variant="text" ripple={false} className="hover:bg-transparent focus:bg-transparent active:bg-transparent">
+                    <IconButton onClick={() => setOpenNav(true)} variant="text" ripple={false} className="hover:bg-transparent focus:bg-transparent active:bg-transparent">
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" stroke="#000" strokeWidth={2}>
                             <path strokeLinecap="round" strokeLinejoin="round" d="M4 6h16M4 12h16M4 18h16" />
                         </svg>
--- components/header/index.js
+++ components/header/index.js
@@ -1,17 +1,39 @@
-import Logo from "./logo"
-import Navbar from "./navbar"
-import Subscribe from "./subscribe"
+import { useState } from "react";
+import { useMediaQuery } from "../../hooks/useMediaQuery";
+import Hamberger from "./hamberger";
+import Logo from "./logo";
+import NavbarList from "./navbar";
+import Notification from "./notification";
+import Subscribe from "./subscribe";
 
 const Header = () => {
-  return (
-    <div className="px-8 ">
-        <header className="flex justify-between items-center py-4 border-b border-gray-400">
-            <Logo/>
-            <Navbar/>
-            <Subscribe/>
-        </header>
-    </div>
-  )
-}
+    const mediaLg = useMediaQuery("lg");
+    const [openNav, setOpenNav] = useState(false)
+    if (mediaLg) {
+        return (
+            <div className="px-8">
+                <header className="flex justify-between items-center py-4 border-b border-gray-400">
+                    <Logo />
+                    <NavbarList />
+                    <Subscribe />
+                </header>
+            </div>
+        );
+    }
+    return (
+        <div className="px-4">
+            <header className="flex flex-col py-4 border-b border-gray-400">
+                <div className="flex justify-between items-center">
+                    <Hamberger openNav={openNav} setOpenNav={setOpenNav}/>
+                    <Logo />
+                    <Notification />
+                </div>
+                <div>
+                    <NavbarList  openNav={openNav}/>
+                </div>
+            </header>
+        </div>
+    );
+};
 
-export default Header
+export default Header;
