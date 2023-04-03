82bef4ecd9b682ef540873a486edc3a35ab42817
hamed donyavi <85428934+donyavirad@users.noreply.github.com>
2023-04-03 13:47:00 +0330

    style (notification): :art: delete icon button and  making icon smaller

--- components/header/notification.js
+++ components/header/notification.js
@@ -1,14 +1,11 @@
-import { IconButton } from "@material-tailwind/react";
 import Link from "next/link";
 import { MdNotifications } from "react-icons/md";
 
 const Notification = () => {
     return (
         <div>
-            <Link href={"/"}>
-                <IconButton variant="text">
-                    <MdNotifications className="w-8 h-8 text-black" />
-                </IconButton>
+            <Link href={"/"} className={"block"}>
+                <MdNotifications className="w-6 h-6 text-black" />
             </Link>
         </div>
     );
