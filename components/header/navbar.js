45a902106165d7f54f54ba5f089fe5469528b6b2
hamed donyavi <85428934+donyavirad@users.noreply.github.com>
2023-04-03 13:59:01 +0330

    style (navbar): :lipstick: change style in mobile display

--- components/header/navbar.js
+++ components/header/navbar.js
@@ -1,6 +1,6 @@
 import { useMediaQuery } from "../../hooks/useMediaQuery";
 
-const NavbarList = ({openNav}) => {
+const NavbarList = ({ openNav }) => {
     const menuItems = ["Hotel Experiences", "Best travel places", "Cheap traveling ideas", "Wildlife Safari", "Best Street food"];
     const mediaLg = useMediaQuery("lg");
     if (mediaLg) {
@@ -21,17 +21,20 @@ const NavbarList = ({openNav}) => {
     }
     return (
         <nav>
-            <ul className="flex flex-col space-y-4">
-                {openNav ? menuItems.map((item, id) => (
-                    <li
-                        key={id}
-                        className="relative transition-all duration-300 font-Lato-Regular text-gray-800  cursor-pointer  hover:text-green-darker hover:font-Lato-Bold after:content-[''] after:absolute after:bottom-0 after:left-0 after:w-0 after:transition-all after:duration-300 after:h-0.5 after:bg-green-darker hover:after:w-1/2 "
-                    >
-                        {item}
-                    </li>
-                )) : ""}
-                
-            </ul>
+            {openNav ? (
+                <ul className="flex flex-col mt-4">
+                    {menuItems.map((item, id) => (
+                        <li
+                            key={id}
+                            className="relative py-1 transition-all duration-300 font-Lato-Regular text-gray-800  cursor-pointer  hover:text-green-darker hover:font-Lato-Bold after:content-[''] after:absolute after:bottom-0 after:left-0 after:w-0 after:transition-all after:duration-300 after:h-0.5 after:bg-green-darker hover:after:w-full "
+                        >
+                            {item}
+                        </li>
+                    ))}
+                </ul>
+            ) : (
+                ""
+            )}
         </nav>
     );
 };
