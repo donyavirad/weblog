60d0a43b55a8c0b32b48f39f15f418a3485da45c
hamed donyavi <85428934+donyavirad@users.noreply.github.com>
2023-04-03 14:09:22 +0330

    style (navbar): :zap: delete navOpen from component

--- components/header/navbar.js
+++ components/header/navbar.js
@@ -1,6 +1,6 @@
 import { useMediaQuery } from "../../hooks/useMediaQuery";
 
-const NavbarList = ({ openNav }) => {
+const NavbarList = () => {
     const menuItems = ["Hotel Experiences", "Best travel places", "Cheap traveling ideas", "Wildlife Safari", "Best Street food"];
     const mediaLg = useMediaQuery("lg");
     if (mediaLg) {
@@ -21,20 +21,16 @@ const NavbarList = ({ openNav }) => {
     }
     return (
         <nav>
-            {openNav ? (
-                <ul className="flex flex-col mt-4">
-                    {menuItems.map((item, id) => (
-                        <li
-                            key={id}
-                            className="relative py-1 transition-all duration-300 font-Lato-Regular text-gray-800  cursor-pointer  hover:text-green-darker hover:font-Lato-Bold after:content-[''] after:absolute after:bottom-0 after:left-0 after:w-0 after:transition-all after:duration-300 after:h-0.5 after:bg-green-darker hover:after:w-full "
-                        >
-                            {item}
-                        </li>
-                    ))}
-                </ul>
-            ) : (
-                ""
-            )}
+            <ul className="flex flex-col mt-4">
+                {menuItems.map((item, id) => (
+                    <li
+                        key={id}
+                        className="relative py-1 transition-all duration-300 font-Lato-Regular text-gray-800  cursor-pointer  hover:text-green-darker hover:font-Lato-Bold after:content-[''] after:absolute after:bottom-0 after:left-0 after:w-0 after:transition-all after:duration-300 after:h-0.5 after:bg-green-darker hover:after:w-full "
+                    >
+                        {item}
+                    </li>
+                ))}
+            </ul>
         </nav>
     );
 };
