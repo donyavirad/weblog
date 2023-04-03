6c93bdfb76e149f2e152f5c3b693d8c1606dc9a8
hamed donyavi <85428934+donyavirad@users.noreply.github.com>
2023-04-03 11:15:12 +0330

    refactor (logo): :zap: Add a small logo and rebuild the logo component

--- components/header/logo.js
+++ components/header/logo.js
@@ -1,9 +1,25 @@
 import Image from "next/image";
+import Link from "next/link";
+import { useMediaQuery } from "../../hooks/useMediaQuery";
 import logo from "../../public/images/logo.svg";
+import smallLogo from "../../public/images/logo-small.svg";
 const Logo = () => {
+    const mediaLg = useMediaQuery("lg");
+
+    if (mediaLg) {
+        return (
+            <div>
+                <Link href={"/"}>
+                    <Image src={logo} className={"w-40"} alt="my blog logo" />
+                </Link>
+            </div>
+        );
+    }
     return (
         <div>
-            <Image src={logo} className={"w-40"} alt="my blog logo" />
+            <Link href={"/"}>
+                <Image src={smallLogo} className={"w-8"} alt="my blog logo" />
+            </Link>
         </div>
     );
 };
