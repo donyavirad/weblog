c7c96166b3d1f0b6b50b07e2b93353509f356f49
hamed donyavi <85428934+donyavirad@users.noreply.github.com>
2023-04-03 12:11:57 +0330

    refactor (navbar): :zap: Change how Nobar displays on mobile screen

--- components/header/navbar.js
+++ components/header/navbar.js
@@ -1,22 +1,39 @@
-const Navbar = () => {
-    const menuItems = [
-        "Hotel Experiences",
-        "Best travel places",
-        "Cheap traveling ideas",
-        "Wildlife Safari",
-        "Best Street food",
-    ]
+import { useMediaQuery } from "../../hooks/useMediaQuery";
+
+const NavbarList = ({openNav}) => {
+    const menuItems = ["Hotel Experiences", "Best travel places", "Cheap traveling ideas", "Wildlife Safari", "Best Street food"];
+    const mediaLg = useMediaQuery("lg");
+    if (mediaLg) {
+        return (
+            <nav>
+                <ul className="flex space-x-4">
+                    {menuItems.map((item, id) => (
+                        <li
+                            key={id}
+                            className="relative transition-all duration-300 font-Lato-Regular text-gray-800  cursor-pointer  hover:text-green-darker hover:font-Lato-Bold after:content-[''] after:absolute after:bottom-0 after:left-0 after:w-0 after:transition-all after:duration-300 after:h-0.5 after:bg-green-darker hover:after:w-1/2 "
+                        >
+                            {item}
+                        </li>
+                    ))}
+                </ul>
+            </nav>
+        );
+    }
     return (
-        <nav className="hidden lg:block">
-            <ul className="flex space-x-4">
-                {menuItems.map((item,id) => (
-                    <li key={id} className="relative transition-all duration-300 font-Lato-Regular text-gray-800  cursor-pointer  hover:text-green-darker hover:font-Lato-Bold after:content-[''] after:absolute after:bottom-0 after:left-0 after:w-0 after:transition-all after:duration-300 after:h-0.5 after:bg-green-darker hover:after:w-1/2 ">
+        <nav>
+            <ul className="flex flex-col space-y-4">
+                {openNav ? menuItems.map((item, id) => (
+                    <li
+                        key={id}
+                        className="relative transition-all duration-300 font-Lato-Regular text-gray-800  cursor-pointer  hover:text-green-darker hover:font-Lato-Bold after:content-[''] after:absolute after:bottom-0 after:left-0 after:w-0 after:transition-all after:duration-300 after:h-0.5 after:bg-green-darker hover:after:w-1/2 "
+                    >
                         {item}
                     </li>
-                ))}
+                )) : ""}
+                
             </ul>
         </nav>
-    )
-  }
-  
-  export default Navbar
+    );
+};
+
+export default NavbarList;
