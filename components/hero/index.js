fdab8e2e9766f3299b061a82b44c108e6b185eaa
hamed donyavi <85428934+donyavirad@users.noreply.github.com>
2023-04-04 18:55:55 +0330

    feat (hero): :sparkles: create hero component and add to home page

---
+++ components/hero/index.js
@@ -0,0 +1,16 @@
+const Hero = () => {
+    return (
+        <div className="flex justify-between items-center">
+            <div className="flex flex-col justify-center items-center">
+                <div>
+                    <span>My Experience Blog</span>
+                </div>
+                <div>casousel</div>
+                <div>search</div>
+            </div>
+            <div>Hero Image</div>
+        </div>
+    );
+};
+
+export default Hero;
