364fb36468af186bceb02993a57556ccd49e48cf
hamed donyavi <85428934+donyavirad@users.noreply.github.com>
2023-04-05 19:55:29 +0330

    style (search): :lipstick: change padding and height. delete w-full from search button

--- components/hero/search.js
+++ components/hero/search.js
@@ -5,10 +5,10 @@ const Search = () => {
         <div className="flex items-center">
             <input
                 type={"text"}
-                className="h-14 py-0 px-4 shadow-box rounded-none rounded-tl-lg rounded-bl-lg font-Montserrat-Regular text-gray-800 focus-visible:outline-none placeholder:text-[#00000020]"
+                className="h-10 px-4 shadow-box rounded-none rounded-tl-lg rounded-bl-lg font-Montserrat-Regular text-gray-800 focus-visible:outline-none placeholder:text-[#00000020]"
                 placeholder="Search Blog"
             />
-            <Button variant="text" color="white" className="h-14 w-full px-10 bg-gradient-to-r from-green-brighter to-green-darker  font-Montserrat-Regular rounded-none rounded-tr-lg rounded-br-lg ">
+            <Button variant="text" color="white" className="h-10 px-8  bg-gradient-to-r from-green-brighter to-green-darker  font-Montserrat-Regular rounded-none rounded-tr-lg rounded-br-lg ">
                 Search
             </Button>
         </div>
