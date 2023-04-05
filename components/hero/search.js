ddb7a4c3124c29e69b0ca8f366a3c2bb1491bdda
hamed donyavi <85428934+donyavirad@users.noreply.github.com>
2023-04-05 20:00:46 +0330

    style (search): :lipstick: add w-full to input in mobile screen

--- components/hero/search.js
+++ components/hero/search.js
@@ -5,7 +5,7 @@ const Search = () => {
         <div className="flex items-center">
             <input
                 type={"text"}
-                className="h-10 px-4 shadow-box rounded-none rounded-tl-lg rounded-bl-lg font-Montserrat-Regular text-gray-800 focus-visible:outline-none placeholder:text-[#00000020]"
+                className="h-10 w-full px-4 shadow-box rounded-none rounded-tl-lg rounded-bl-lg font-Montserrat-Regular text-gray-800 focus-visible:outline-none placeholder:text-[#00000020] lg:w-auto"
                 placeholder="Search Blog"
             />
             <Button variant="text" color="white" className="h-10 px-8  bg-gradient-to-r from-green-brighter to-green-darker  font-Montserrat-Regular rounded-none rounded-tr-lg rounded-br-lg ">
