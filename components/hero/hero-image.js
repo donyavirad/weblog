ba4b60721dae76433efe5c4f619cdbb7dac6fa34
hamed donyavi <85428934+donyavirad@users.noreply.github.com>
2023-04-05 19:43:21 +0330

    style (hero-image): :lipstick: delete style in lg screen size

--- components/hero/hero-image.js
+++ components/hero/hero-image.js
@@ -1,11 +1,11 @@
-import Image from "next/image"
-import heroImg from "../../public/images/hero.svg"
+import Image from "next/image";
+import heroImg from "../../public/images/hero.svg";
 const HeroImage = () => {
-  return (
-    <div className="w-full lg:w-1/2 ">
-        <Image src={heroImg} alt="hero image"/>
-    </div>
-  )
-}
+    return (
+        <div>
+            <Image src={heroImg} alt="hero image" />
+        </div>
+    );
+};
 
-export default HeroImage
+export default HeroImage;
