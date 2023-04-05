c94a572961112491271933ee69494a350dfc69aa
hamed donyavi <85428934+donyavirad@users.noreply.github.com>
2023-04-05 18:07:37 +0330

    style (search, tailwind.config): :art: add shadow to input search component

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,30 +2,33 @@
 const withMT = require("@material-tailwind/react/utils/withMT");
 
 module.exports = withMT({
-  content: [
-    "./app/**/*.{js,ts,jsx,tsx}",
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
- 
-    // Or if using `src` directory:
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {
-        colors:{
-            green: {
-                brighter: "#1DE54F",
-                darker: "#36BE58"
+    content: [
+        "./app/**/*.{js,ts,jsx,tsx}",
+        "./pages/**/*.{js,ts,jsx,tsx}",
+        "./components/**/*.{js,ts,jsx,tsx}",
+
+        // Or if using `src` directory:
+        "./src/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+        extend: {
+            colors: {
+                green: {
+                    brighter: "#1DE54F",
+                    darker: "#36BE58",
+                },
+            },
+            fontFamily: {
+                "Montserrat-Regular": "Montserrat-Regular",
+                "Montserrat-Medium": "Montserrat-Medium",
+                "Montserrat-Bold": "Montserrat-Bold",
+                "Lato-Bold": "Lato-Bold",
+                "Lato-Regular": "Lato-Regular",
+            },
+            boxShadow: {
+                box: "0 3px 30px 0  #00000016",
             },
         },
-        fontFamily: {
-            "Montserrat-Regular": "Montserrat-Regular",
-            "Montserrat-Medium": "Montserrat-Medium",
-            "Montserrat-Bold": "Montserrat-Bold",
-            "Lato-Bold": "Lato-Bold",
-            "Lato-Regular": "Lato-Regular",
-        }
-    }
-  },
-  plugins: [],
-})
+    },
+    plugins: [],
+});
