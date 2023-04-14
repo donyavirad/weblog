2423ae5988f854da1818be1651591b67fa8c295c
hamed donyavi <85428934+donyavirad@users.noreply.github.com>
2023-04-14 18:56:50 +0330

    feat (posts): :sparkles: create posts.js file and add to home page.

---
+++ components/posts/posts.js
@@ -0,0 +1,14 @@
+const Posts = () => {
+    return (
+        <div>
+            <div>
+                posts
+            </div>
+            <div>
+                Load more
+            </div>
+        </div>
+    )
+}
+
+export default Posts
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,13 @@
 import Header from "../components/header";
 import Hero from "../components/hero";
+import Posts from "../components/posts/posts";
 
 export default function Home() {
   return (
     <div>
         <Header/>
         <Hero/>
+        <Posts/>
     </div>
   )
 }
