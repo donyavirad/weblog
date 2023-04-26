2821b42af4539dbe39d4baa1f6e63975f822587b
hamed donyavi <85428934+donyavirad@users.noreply.github.com>
2023-04-26 10:22:01 +0330

    feat: (data/posts) :sparkles: add posts data array with images to project

---
+++ data/posts.js
@@ -0,0 +1,32 @@
+export const posts = [
+    {
+        id: 1,
+        title: "Meeting new faces",
+        summery: "Later, when I would meet Binna and learn about his Port Douglas, I’d think back to these modern and contemporary canvases.  ",
+        category: "category tag",
+        image: {
+            url: "/images/meeting-new-faces.jpg",
+            alt: "Meeting new faces",
+        }
+    },
+    {
+        id: 2,
+        title: "Live the island life: 20 unique islands",
+        summery: " have no shame in admitting I’m an addict for the island life, but as someone who tires quickly of lazy days in the sand, I’m often looking for something a bit different.",
+        category: "category tag",
+        image: {
+            url: "/images/uniq-islands.jpg",
+            alt: "Live the island life: 20 unique islands",
+        }
+    },
+    {
+        id: 3,
+        title: "Solo in Seville: a weekend in Spain",
+        summery: "Seville is one of the most distinctive cities I’ve ever visited. There is something about it that will captivate you, and it’s ideal for solo travellers.",
+        category: "category tag",
+        image: {
+            url: "/images/weekend-in-spain.jpg",
+            alt: "Solo in Seville: a weekend in Spain",
+        }
+    },
+]
