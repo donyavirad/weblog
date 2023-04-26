18d348f6da93004432aec791f18cba90124f666b
hamed donyavi <85428934+donyavirad@users.noreply.github.com>
2023-04-26 20:42:03 +0330

    fix (components/header/menu-icon): :bug: delete ripple and variant attribute from button element.

--- components/header/menu-icon.js
+++ components/header/menu-icon.js
@@ -7,13 +7,13 @@ const Hamberger = ({ openNav, setOpenNav }) => {
         return (
             <div className="flex justify-center items-center">
                 {openNav ? (
-                    <button onClick={() => setOpenNav(false)} variant="text" ripple={false} className="hover:bg-transparent focus:bg-transparent active:bg-transparent">
+                    <button onClick={() => setOpenNav(false)} className="hover:bg-transparent focus:bg-transparent active:bg-transparent">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" className="h-6 w-6" viewBox="0 0 24 24" stroke="#000" strokeWidth={2}>
                             <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
                         </svg>
                     </button>
                 ) : (
-                    <button onClick={() => setOpenNav(true)} variant="text" ripple={false} className="hover:bg-transparent focus:bg-transparent active:bg-transparent">
+                    <button onClick={() => setOpenNav(true)} className="hover:bg-transparent focus:bg-transparent active:bg-transparent">
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" stroke="#000" strokeWidth={2}>
                             <path strokeLinecap="round" strokeLinejoin="round" d="M4 6h16M4 12h16M4 18h16" />
                         </svg>
